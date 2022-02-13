fb5d94b2d86079c0a8b3f2925b18cd9b9d5ec2b8
k-roz <k-roz@DESKTOP-BMAD82K>
2022-02-13 23:07:28 +0300

    Добавьте файлы проекта.

---
+++ LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+            // создаем новый список для результатов
+            var selectedPeople = from p in people // передаем каждый элемент из people в переменную p
+                                 where p.ToUpper().StartsWith("T") //фильтрация по критерию
+                                 orderby p  // упорядочиваем по возрастанию
+                                 select p; // выбираем объект в создаваемую коллекцию
+
+            foreach (string person in selectedPeople)
+                Console.WriteLine(person);
+            Console.ReadKey();
+
+        }
+    }
+}
